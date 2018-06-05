bbb4f27d6f196a72f98901e8cec0a95200bc3d84
Amogh M Aradhya <amoghmanjunath99@gmail.com>
2018-06-05 09:25:37 +0530

    Add files via upload
    Parameters added

---
+++ KWh_Meter_v0.1.ino
@@ -0,0 +1,182 @@
+//Libraries
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+
+//WiFi AP Connection
+#define wifi_ssid "ERROR1749 #DDOSinc"
+#define wifi_password "n150wireless"
+
+
+
+//Topics
+const char* mqtt_topic_watt = "ESP-energy-01/watt";
+const char* mqtt_topic_kwh = "ESP-energy-01/kwh";
+const char* mqtt_topic_rate = "ESP-energy-01/rate";
+
+
+//MQTT Client Setup
+WiFiClient espClient;
+PubSubClient client(espClient);
+#define mqtt_server "192.168.2.120"
+
+
+
+const int Sensor_Pin = A0;
+unsigned int Sensitivity = 66;   // 185mV/A for 5A, 100 mV/A for 20A and 66mV/A for 30A Module
+float Vpp = 0; // peak-peak voltage 
+float Vrms = 0; // rms voltage
+float Irms = 0; // rms current
+float Supply_Voltage = 233.0;           // reading from DMM
+float Vcc = 5.0;         // ADC reference voltage // voltage at 5V pin 
+float power = 0;         // power in watt              
+float Wh =0 ;             // Energy in kWh
+unsigned long last_time =0;
+unsigned long current_time =0;
+unsigned long interval = 100;
+unsigned int calibration = 100;  // V2 slider calibrates this
+unsigned int pF = 85;           // Power Factor default 95
+float bill_amount = 0;   // 30 day cost as present energy usage incl approx PF 
+unsigned int energyTariff = 8.0; // Energy cost in INR per unit (kWh)
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n Rebooted");
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  
+}
+
+
+
+String macToStr(const uint8_t* mac)
+{
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5)
+      result += ':';
+  }
+  return result;
+}
+
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+    
+          // Generate client name based on MAC address and last 8 bits of microsecond counter
+      String clientName;  
+      clientName += "esp8266-";
+      uint8_t mac[6];
+      WiFi.macAddress(mac);
+      clientName += macToStr(mac);
+      clientName += "-";
+      clientName += String(micros() & 0xff, 16);
+      Serial.print("Connecting to ");
+      Serial.print(mqtt_server);
+      Serial.print(" as ");
+      Serial.println(clientName);
+
+
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+  if (client.connect((char*) clientName.c_str())) {
+    //if (client.connect((char*) clientName.c_str()), mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+        reconnect();
+      }
+      client.loop();
+  getACS712();
+      // Wait a few seconds between measurements.
+      delay(2000);
+
+}
+
+
+
+void getACS712() {  // for AC
+  Vpp = getVPP();
+  Vrms = (Vpp/2.0) *0.707; 
+  Vrms = Vrms - (calibration / 10000.0);     // calibtrate to zero with slider
+  Irms = (Vrms * 1000)/Sensitivity ;
+  if((Irms > -0.015) && (Irms < 0.008)){  // remove low end chatter
+    Irms = 0.0;
+  }
+  power= (Supply_Voltage * Irms) * (pF / 100.0); 
+  last_time = current_time;
+  current_time = millis();    
+  Wh = Wh+  power *(( current_time -last_time) /3600000.0) ; // calculating energy in Watt-Hour
+  bill_amount = Wh * (energyTariff/1000);
+  Serial.print("Irms:  "); 
+  Serial.print(String(Irms, 3));
+  Serial.println(" A");
+  Serial.print("Power: ");   
+  Serial.print(String(power, 3)); 
+  Serial.println(" W"); 
+  Serial.print("  Bill Amount: INR"); 
+  Serial.println(String(bill_amount, 2));
+  client.publish(mqtt_topic_watt, String(Irms).c_str(), true);
+  client.publish(mqtt_topic_kwh, String(power).c_str(), true);
+  client.publish(mqtt_topic_rate, String(bill_amount).c_str(), true);
+  
+}
+
+float getVPP()
+{
+  float result; 
+  int readValue;                
+  int maxValue = 0;             
+  int minValue = 1024;          
+  uint32_t start_time = millis();
+  while((millis()-start_time) < 950) //read every 0.95 Sec
+  {
+     readValue = analogRead(Sensor_Pin);    
+     if (readValue > maxValue) 
+     {         
+         maxValue = readValue; 
+     }
+     if (readValue < minValue) 
+     {          
+         minValue = readValue;
+     }
+  } 
+   result = ((maxValue - minValue) * Vcc) / 1024.0;  
+   return result;
+ }
+ 
